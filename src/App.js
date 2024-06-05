f8a6bdad84e03a7b6b3e4b428b60cffa9bfabb1b
Maiara Luiza <maipluiza@hotmail.com>
2024-06-04 21:15:15 -0300

    projeto inicial do organo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Banner from "./componentes/Banner/Banner.js";
+import Formulario from "./componentes/Formulario/index.js";
+import Time from "./componentes/Time/index.js";
+import Rodape from "./componentes/Rodape/index.js";
 
 function App() {
+
+  const times = [
+    {
+      nome:'Programação',
+      corPrimaria:'#57c278',
+      corSecundaria:'#d9f7e9'
+    },
+    {
+      nome:'Front-End',
+      corPrimaria:'#82cffa',
+      corSecundaria:'#e8f8ff'
+    },
+    {
+      nome:'Data Science',
+      corPrimaria:'#a6d157',
+      corSecundaria:'#f0f8e2'
+    },
+    {
+      nome:'DevOps',
+      corPrimaria:'#e06b69',
+      corSecundaria:'#fde7e8'
+    },
+    {
+      nome:'UX e Design',
+      corPrimaria:'#d86ebf',
+      corSecundaria:'#fae9f5'
+    },
+    {
+      nome:'Mobile',
+      corPrimaria:'#ffba05',
+      corSecundaria:'#fff5d9'
+    },
+    {
+      nome:'Inovação e Gestão',
+      corPrimaria:'#ff8a29',
+      corSecundaria:'#ffeedf'
+    }
+  ]
+
+  const [colaboradores, setColaboradores] = useState([])
+  
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner/>
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}     
+      <Rodape/>
     </div>
   );
 }
